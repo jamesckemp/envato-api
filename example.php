86b53cb53270c7cc2b801cfb70cec0f884f65406
James Kemp <me@jckemp.com>
2016-02-09 21:57:18 +0000

    Update example.php

--- example.php
+++ example.php
@@ -4,5 +4,7 @@ include('class-envato-api.php');
 
 $envato = new Envato_API('username', 'personal_token');
 $item = $envato->item_details( '13523915' );
+$prices = $envato->item_prices( '13523915' );
 
 $envato->pretty_print( $item ); // prints all the juicy item details, including plugin/theme meta
+$envato->pretty_print( $prices ); // prints regular and extended prices
